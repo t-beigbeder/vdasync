8b12f98b7dd91463562f02d3a8aea6c840e3a6f2
thierry_otvl <tbeigbeder.nospam@gmail.com>
2026-04-18 17:22:22 +0000

    mc

--- internal/grpc/tests.go
+++ internal/grpc/tests.go
@@ -1,20 +1,50 @@
 package grpc
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/t-beigbeder/otvl_dtacsy/internal/common"
+	"github.com/t-beigbeder/otvl_dtacsy/opegrpc"
+	"google.golang.org/grpc"
+)
 
 const testHost = "localhost"
 
-func RunGrpcTestServer() (string, context.CancelFunc, error) {
+type opeServer struct {
+	opegrpc.UnimplementedOpeServer
+}
+
+func (s *opeServer) Ready(context.Context, *opegrpc.Empty) (*opegrpc.Bool, error) {
+	return &opegrpc.Bool{Value: true}, nil
+}
+func (s *opeServer) Version(context.Context, *opegrpc.Empty) (*opegrpc.Value, error) {
+	return &opegrpc.Value{Value: "0.1"}, nil
+}
+func (s *opeServer) Shutdown(context.Context, *opegrpc.Value) (*opegrpc.Bool, error) {
+	return &opegrpc.Bool{Value: true}, nil
+}
+
+func RunGrpcTestServer() (int, context.CancelFunc, error) {
 	ctx, cCancel := context.WithCancel(context.Background())
 	_ = ctx // FIXME
 	var (
 		err  error
-		port string
+		port int
+		opts []grpc.ServerOption
 	)
 	defer func() {
 		if err != nil {
 			cCancel()
 		}
 	}()
+	if port, err = common.GetFreePort(); err != nil {
+		return port, cCancel, err
+	}
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", testHost, port))
+	_ = lis // FIXME
+	grpcServer := grpc.NewServer(opts...)
+	opegrpc.RegisterOpeServer(grpcServer, &opeServer{})
 	return port, cCancel, nil
 }
