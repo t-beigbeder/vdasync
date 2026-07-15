f4cf5ef1859cb9c48079dd7020d91b4084b4fb02
thierry_otvl <tbeigbeder.nospam@gmail.com>
2026-07-15 18:25:06 +0200

    list always in utc

--- internal/common/dssadapt.go
+++ internal/common/dssadapt.go
@@ -165,7 +165,7 @@ func DataEntryList(de *dssa.DataEntry, isNoOwn bool, cs string) string {
 	}
 	return fmt.Sprintf("%s%s%s%s%s%10d %21s %s%s%s", tp,
 		rightsList(de.UserRights), rightsList(de.GroupRights), rightsList(de.OtherRights),
-		ownDisp, de.Size, time.Unix(de.Mtime, 0).Format(time.RFC3339), de.Path,
+		ownDisp, de.Size, time.Unix(de.Mtime, 0).UTC().Format(time.RFC3339), de.Path,
 		lt, csd,
 	)
 }
