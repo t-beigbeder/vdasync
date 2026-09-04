3848ecdc94edef7970b1e181a7c07573f95dba5b
thierry_otvl <tbeigbeder.nospam@gmail.com>
2026-09-04 15:40:35 +0000

    cleanup done

--- opelog/queue.go
+++ opelog/queue.go
@@ -1,11 +1,5 @@
 package opelog
 
-type QueueV0 interface {
-	Enqueue(string) error
-	Dequeue() (string, error)
-	Close() error
-}
-
 type Queue interface {
 	Put(string) error
 	Get() (string, error)
